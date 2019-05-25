84fc057c3f1965eafc476075e74942e838be78d6
Ximena Pinacho <ximena_pinacho@icloud.com>
2019-05-24 20:11:28 -0500

    logo blanco

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,13 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <btnCute/>
 </template>
+<script>
+  import btnCute from "../components/button.vue"
+
+  export default {
+    components: {
+      btnCute
+    }
+  }
+</script>
+
